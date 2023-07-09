068355657e215983dba593a4b239b84e9012929c
PEPIDGE <pepkatabrat123@gmail.com>
2023-07-09 12:25:51 +0300

    Social platform links added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,6 +23,20 @@ export const Header = ({ boolean }) => {
           </li>
         </ul>
         <div className="number">0884 686 895 </div>
+        <div className="social-links-container">
+          <Link className="social-link" to={"https://www.instagram.com/bg_drone_footage/"} target="_blank">
+            <ion-icon id={boolean ? "black" : "white"} name="logo-instagram"></ion-icon>
+          </Link>
+          <Link className="social-link" to={"https://www.facebook.com/profile.php?id=100093293493392"} target="_blank">
+            <ion-icon id={boolean ? "black" : "white"} name="logo-facebook"></ion-icon>
+          </Link>
+          <Link className="social-link" to={"https://www.tiktok.com/@bg_drone_footage"} target="_blank">
+            <ion-icon id={boolean ? "black" : "white"} name="logo-tiktok"></ion-icon>
+          </Link>
+          <Link className="social-link" to={"https://youtube.com/@BGDroneFootage"} target="_blank">
+            <ion-icon id={boolean ? "black" : "white"} name="logo-youtube"></ion-icon>
+          </Link>
+        </div>
       </nav>
     </>
   );
