c224acd8e346ff1e7a9a7bff39373cdc030d2d90
PEPIDGE <pepkatabrat123@gmail.com>
2023-07-11 23:31:17 +0300

    Business cards added

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
 import "../css-components/HomePage.css";
 import { Link } from "react-router-dom";
 import { Gallery } from "./Gallery";
 import { Header } from "./Header";
+import React, { useEffect, useState } from "react";
+
 export const HomePage = () => {
   const boolean = false;
 
@@ -25,21 +26,49 @@ export const HomePage = () => {
       </div>
 
       <div class="card-container">
+        <h1 className="title">Защо да изберете нас?</h1>
         <div class="content-section">
           <div class="card">
-            <img src="./photos/DJI_0147.jpg" />
+            <img className="quality" src="./photos/DJI_0147.jpg" />
             <h2>Качество</h2>
             <p>4К HDR видеа</p>
           </div>
           <div class="card">
             <img src="./photos/DJI_0147.jpg" />
-            <h2>Видеообработка</h2>
-            <p>Ние предлагаме видеообработка на поредица от видеа и снимки в сюжетна линия</p>
+            <h2>Фотография</h2>
+            <p>
+              Ние предлагаме висококачествена фотография с 48 MP сензор 8064 x
+              6048 пиксела{" "}
+            </p>
           </div>
+
           <div class="card">
-            <img src="./photos/DJI_0147.jpg" />
+            <img className="quality" src="./photos/DJI_0147.jpg" />
             <h2>Времетраене</h2>
             <p>Над 120 минути във въздуха</p>
+          </div>
+
+          <div class="content-section">
+            <div class="card">
+              <img src="./photos/DJI_0147.jpg" />
+              <h2>Видеообработка</h2>
+              <p>
+                Ние предлагаме видеообработка на поредица от видеа и снимки в
+                сюжетна линия
+              </p>
+            </div>
+            <div class="card">
+              <img src="./photos/DJI_0147.jpg" />
+              <h2>360 градусова панорама</h2>
+              <p>Ние предлагаме 360 градусова реалност в снимка </p>
+            </div>
+            <div class="card">
+              <img src="./photos/DJI_0147.jpg" />
+              <h2>
+                Вертикални видеа <br /> и снимки
+              </h2>
+              <p>Видеа и снимки за социалните платформи</p>
+            </div>
           </div>
         </div>
       </div>
