460b64b4742a20e151d7e04f00a6cc9b3b62ad7f
PEPIDGE <pepkatabrat123@gmail.com>
2023-08-04 20:10:46 +0300

    trying to fix error

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HelmetProvider } from "react-helmet-async";
 import { Analytics } from "@vercel/analytics/react";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Analytics>
-    <HelmetProvider>
+ <>
+  <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
-  </Analytics>
+      <Analytics/>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
