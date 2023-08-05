797fac7b477e8daad8b93d6038dcc672e1f31f3f
PEPIDGE <pepkatabrat123@gmail.com>
2023-08-05 10:07:19 +0300

    Updated css and added buttons on the card with page links

--- client/src/page-components/SessionsPage.js
+++ client/src/page-components/SessionsPage.js
@@ -3,17 +3,12 @@ import "../css-components/App.css";
 import "../css-components/Services.css";
 import "../css-components/Sessions.css";
 import { Header } from "./Header";
-import { useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 
 export const SessionsPage = () => {
   const boolean = true;
-  const navigate = useNavigate();
 
-  const navigateToSession = (id) => {
-
-    navigate(`/sessions/${id}`);
-  }
 
   return (
     <>
@@ -27,31 +22,40 @@ export const SessionsPage = () => {
         <h1 className="title title-sessions">Нашите сесии</h1>
         <div className="card-container sessions" >
           <div className="content-section " >
-            <div className="card card-session" onClick={navigateToSession.bind(null, "vila-Gingerlandiq")}>
-              <img src="https://i.postimg.cc/wv6XN1f2/DJI-0050.jpg" className="taller-image"/>
+            <div className="card card-session">
+              <img src="https://i.postimg.cc/wv6XN1f2/DJI-0050.jpg" className="quality"/>
              <div className="text-session">
 
               <h2>Вила Гингерландия</h2>
               <p>
                 Ние направихме въздушни кадри на една прекрасна вила.
               </p>
+              <div className="show-more-wrap">
+                 <Link to={"vila-Gingerlandiq"} className="red-button show-more">Виж още</Link>
+              </div>
              </div>
             </div>
-            <div className="card card-session" onClick={navigateToSession.bind(null, "plevenska-panorama")}>
-              <img src="https://i.postimg.cc/qq26Z05q/DJI-0254.jpg" className="taller-image"/>
+            <div className="card card-session">
+              <img src="https://i.postimg.cc/qq26Z05q/DJI-0254.jpg" className="quality"/>
              <div className="text-session">
 
               <h2>Плевенската епопея</h2>
               <p>Направихме зашеметяващи кадри на Плевенската панорама.</p>
+              <div className="show-more-wrap">
+                 <Link to={"plevenska-panorama"} className="red-button show-more">Виж още</Link>
+              </div>
             </div>
 
             </div>
 
-            <div className="card card-session" onClick={navigateToSession.bind(null, "tylenovo")}>
-              <img src="https://i.postimg.cc/jjRwgGHd/DJI-0095.jpg" />
+            <div className="card card-session">
+              <img src="https://i.postimg.cc/jjRwgGHd/DJI-0095.jpg" className="quality" />
              <div className="text-session">
              <h2>Тюленово</h2>
               <p>Ние снимахме прекрасните скали около село Тюленово.</p>
+              <div className="show-more-wrap">
+                 <Link to={"tylenovo"} className="red-button show-more">Виж още</Link>
+              </div>
              </div>
             </div>
 
